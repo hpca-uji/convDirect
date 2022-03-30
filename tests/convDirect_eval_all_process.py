033f2a1badf88f474a9885b7b9f4ecb9907c2e39
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2022-03-30 22:09:49 +0200

    Improved convDirect_eval_all_process.py and renamed the CNNs to their usual capitalization

--- tests/convDirect_eval_all_process.py
+++ tests/convDirect_eval_all_process.py
@@ -12,6 +12,9 @@ best_dfs = {}
 blis_dfs = {}
 shalom_dfs = {}
 
+# Follow the nomenclature used on "Reformulating the Direct Convolution for High-Performance on ARM Processors" article
+for_article = True
+
 
 def my_help():
     print("""
@@ -20,7 +23,8 @@ Usage: {} INPUT_DIR [SUFFIX]
 where:
  INPUT_DIR is the input directory, usually 'convdirect_output_hostname'.
  SUFFIX is the suffix that will be used on the plots titles, if this parameter is
-        not provided, 'performance on Hostname' will be used.
+        not provided, 'performance on Hostname' will be used. To generate an empty
+        suffix, "" can be used.
  
     """.format(sys.argv[0]))
 
@@ -33,7 +37,7 @@ def process_args():
 
     input_dir = sys.argv[1]
     if len(sys.argv) == 3:
-        plots_titles_suffix = sys.argv[2]
+        plots_titles_suffix = sys.argv[2].strip()
     else:
         host_info = "Unknown"
         host_info_file = os.path.join(input_dir, "host.nfo")
@@ -51,9 +55,18 @@ def process_args():
         plots_titles_suffix = "performance on {}".format(host_info)
 
 
-(IM2COL, CONVGEMM, BLIS_8X12, BLIS_4X20, BLIS_BLIS,
- SHALOM_7x12, SHALOM_6x16, TZEMENG) = ("Im2col", "ConvGemm", "Blis_mk-8x12", "Blis_mk-4x20", "Blis_mk-blis",
-                                       "Shalom-7x12", "Shalom-6x16", "Tze-Meng")
+if not for_article:
+    (IM2COL, CONVGEMM, TZEMENG,
+     SHALOM_7x12, SHALOM_6x16,
+     BLIS_8X12, BLIS_4X20, BLIS_BLIS,) = ("Im2col", "ConvGemm", "Tze-Meng",
+                                          "Shalom (7x12)", "Shalom (6x16)",
+                                          "BLIS (8x12)", "BLIS (4x20)", "BLIS (BLIS)")
+else:
+    (IM2COL, CONVGEMM, TZEMENG,
+     SHALOM_7x12, SHALOM_6x16,
+     BLIS_8X12, BLIS_4X20, BLIS_BLIS,) = ("Im2col", "ConvGemm", "BLOCKED",
+                                          "NEW-A (7x12)", "NEW-A (6x16)",
+                                          "NEW-B (8x12)", "NEW-B (4x20)", "NEW-B (BLIS)")
 
 
 def performance_results():
@@ -63,14 +76,14 @@ def performance_results():
     method_from = {
         "convdirect_im2row_nhwc_default": IM2COL,
         "convdirect_conv_gemm_nhwc_default": CONVGEMM,
+        "convdirect_tzemeng_nhwc_7x12_u4": TZEMENG,
+        "convdirect_block_shalom_nhwc_7x12_npa_u4": SHALOM_7x12,
+        "convdirect_block_shalom_nhwc_6x16_npa_u4": SHALOM_6x16,
         "convdirect_block_blis_nhwc_8x12": BLIS_8X12,
         "convdirect_block_blis_nhwc_4x20": BLIS_4X20,
         "convdirect_block_blis_nhwc_blis": BLIS_BLIS,
-        "convdirect_block_shalom_nhwc_7x12_npa_u4": SHALOM_7x12,
-        "convdirect_block_shalom_nhwc_6x16_npa_u4": SHALOM_6x16,
-        "convdirect_tzemeng_nhwc_7x12_u4": TZEMENG,
     }
-    cols_ordered = [IM2COL, CONVGEMM, SHALOM_7x12, SHALOM_6x16, BLIS_8X12, BLIS_4X20, BLIS_BLIS, TZEMENG]
+    cols_ordered = [IM2COL, CONVGEMM, TZEMENG, SHALOM_7x12, SHALOM_6x16, BLIS_8X12, BLIS_4X20, BLIS_BLIS]
 
     try:
         import pandas as pd
@@ -89,6 +102,7 @@ def performance_results():
     for filename in glob.glob(os.path.join(input_dir, "*.csv")):
         print("Importing '{}'".format(filename))
         net, algorithm = os.path.basename(filename)[:-4].split("_-_")
+        net = net.replace("-", " ")
         if net not in nets:
             nets.append(net)
         try:
@@ -115,26 +129,22 @@ def performance_results():
         cols_present = df_net.columns.tolist()
         df_net = df_net[[c for c in cols_ordered if c in cols_present]]
         # Best(Im2col, ConvGemm)
-        im2col_convgemm_label = "Best(Im2col, ConvGemm)"
+        im2col_convgemm_label = "LOWERING" if for_article else "Best(Im2col, ConvGemm)"
         df_net[im2col_convgemm_label] = df_net[[IM2COL, CONVGEMM]].max(axis=1)
-        # Best of BLIS methods
-        blis_cols = [c for c in [BLIS_8X12, BLIS_4X20, BLIS_BLIS] if c in cols_present]
-        blis_label = None
-        if len(blis_cols) > 1:
-            blis_label = "Best({})".format(", ".join(blis_cols))
-            df_net[blis_label] = df_net[blis_cols].max(axis=1)
-        elif len(blis_cols) == 1:
-            blis_label = blis_cols[0]
         # Best of Shalom methods
         shalom_cols = [c for c in [SHALOM_7x12, SHALOM_6x16] if c in cols_present]
         shalom_label = None
-        if len(shalom_cols) > 1:
-            shalom_label = "Best({})".format(", ".join(shalom_cols))
+        if len(shalom_cols) >= 1:
+            shalom_label = "NEW-A" if for_article else "Best({})".format(", ".join(shalom_cols))
             df_net[shalom_label] = df_net[shalom_cols].max(axis=1)
-        elif len(shalom_cols) == 1:
-            shalom_label = shalom_cols[0]
+        # Best of BLIS methods
+        blis_cols = [c for c in [BLIS_8X12, BLIS_4X20, BLIS_BLIS] if c in cols_present]
+        blis_label = None
+        if len(blis_cols) >= 1:
+            blis_label = "NEW-B" if for_article else "Best({})".format(", ".join(blis_cols))
+            df_net[blis_label] = df_net[blis_cols].max(axis=1)
         # ---
-        basename = os.path.join(output_dir, net)
+        basename = os.path.join(output_dir, net.replace(" ", "_"))
         df_net.to_csv("{}.csv".format(basename), sep=";")
         df_net.to_markdown("{}.txt".format(basename))
         try:
@@ -178,13 +188,13 @@ def plots_matplotlib():
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    def bars(df, plot_title, filename):
-        print("Generating '{}'...".format(filename))
+    def bars(df_, plot_title_, filename_, colors_):
+        print("Generating '{}'...".format(filename_))
         # Labels
-        labels = df["#Layer"].to_list()
+        labels = df_["#Layer"].to_list()
         x = np.arange(len(labels))  # the label locations
         # Methods
-        methods = [c for c in df.columns.to_list() if c != "#Layer"]
+        methods = [c for c in df_.columns.to_list() if c != "#Layer"]
         n_methods = len(methods)
         # Create figure
         figure_width = 11  # a4 paper is 11-3/4 inches width
@@ -195,13 +205,13 @@ def plots_matplotlib():
         total_bar_width = 0.8
         bar_width = total_bar_width / n_methods
         # See matplotlib color maps in https://matplotlib.org/stable/tutorials/colors/colormaps.html
-        cm = plt.cm.get_cmap('tab20b')
+        # cm = plt.cm.get_cmap('tab20b')
         ic = len(methods) / 2 - .5  # i centered
         for i, method in enumerate(methods):
-            values = df[method].to_list()
-            plt.bar(x + (i - ic) * bar_width, values, bar_width, label=method, zorder=3, color=cm.colors[i])
+            values = df_[method].to_list()
+            plt.bar(x + (i - ic) * bar_width, values, bar_width, label=method, zorder=3, color=colors_[i])
         # Add title, legend, labels, etc.
-        plt.title(plot_title)
+        plt.title(plot_title_)
         plt.legend(loc="upper left", fontsize=8, ncol=n_methods)
         plt.margins(x=0.005, y=0.2, tight=True)
         plt.grid(True, zorder=0)
@@ -209,22 +219,40 @@ def plots_matplotlib():
         plt.xlabel("#CNN layer")
         plt.xticks(x, labels, fontsize=8, rotation=60)
         # Save figure
-        plt.savefig(filename)
+        plt.savefig(filename_)
+        # Close figure
+        plt.close()
 
+    def split(df_):
+        parts = len(df_.index) // 25 + 1
+        if parts == 1:
+            return [(df_, ""), ]
+        else:
+            return [(x, str(i)) for i, x in enumerate(np.array_split(df_, parts), 1)]
+
+    def draw_bars(net_, df_, prefix_, plots_titles_suffix_, output_dir_, colors_):
+        for df_part, suffix in split(df_):
+            title_parts = [x for x in (net_, plots_titles_suffix_, suffix) if x != ""]
+            plot_title = " ".join(title_parts)
+            plot_title_escaped = plot_title.replace(" ", "_").replace("(", "").replace(")", "")
+            filename = "{}_{}.pdf".format(prefix_, plot_title_escaped).lower()
+            bars(df_part, plot_title, os.path.join(output_dir_, filename), colors_)
+
+    cm = plt.cm.get_cmap('tab10')
+    colors = [cm.colors[x] for x in range(4)]
+    colors.reverse()
     for net, df in best_dfs.items():
-        plot_title = "{} {}".format(net, plots_titles_suffix)
-        filename = "best_{}.pdf".format(plot_title.replace(" ", "_").replace("(", "").replace(")", "")).lower()
-        bars(df, plot_title, os.path.join(output_dir, filename))
+        draw_bars(net, df, "best", plots_titles_suffix, output_dir, colors)
 
+    cm = plt.cm.get_cmap('tab20c')
+    colors = [cm.colors[x] for x in range(3)]
     for net, df in blis_dfs.items():
-        plot_title = "{} {}".format(net, plots_titles_suffix)
-        filename = "blis_{}.pdf".format(plot_title.replace(" ", "_").replace("(", "").replace(")", "")).lower()
-        bars(df, plot_title, os.path.join(output_dir, filename))
+        draw_bars(net, df, "blis", plots_titles_suffix, output_dir, colors)
 
+    cm = plt.cm.get_cmap('tab20c')
+    colors = [cm.colors[4 + x] for x in range(3)]
     for net, df in shalom_dfs.items():
-        plot_title = "{} {}".format(net, plots_titles_suffix)
-        filename = "shalom_{}.pdf".format(plot_title.replace(" ", "_").replace("(", "").replace(")", "")).lower()
-        bars(df, plot_title, os.path.join(output_dir, filename))
+        draw_bars(net, df, "shalom", plots_titles_suffix, output_dir, colors)
 
     print()
     print("The plots are in '{}'\n".format(os.path.abspath(output_dir)))
